09f5f1d36ea606229b15a21cf63a9c95eb304f78
Ajith Kumar E <ajithkumar.e1705@gmail.com>
2026-08-06 02:55:59 +0000

    updated import tf file

--- Terraform/terraform-import/main.tf
+++ Terraform/terraform-import/main.tf
@@ -1,8 +1,74 @@
-provider aws {
-  region = "ap-southeast-1"
+provider "aws" {
+  region = "ap-south-1"
 }
-
-import {
-    id = "i-0c4a18c208b251abd"
-    to = aws_instance.my_instance
+resource "aws_instance" "my_instance" {
+  ami                                  = "ami-035827357e3c7e810"
+  associate_public_ip_address          = true
+  availability_zone                    = "ap-south-1b"
+  disable_api_stop                     = false
+  disable_api_termination              = false
+  ebs_optimized                        = true
+  force_destroy                        = false
+  get_password_data                    = false
+  hibernation                          = false
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t3.micro"
+  key_name                             = "ak"
+  monitoring                           = false
+  placement_partition_number           = 0
+  private_ip                           = "172.31.3.47"
+  region                               = "ap-south-1"
+  secondary_private_ips                = []
+  security_groups                      = ["launch-wizard-1"]
+  source_dest_check                    = true
+  subnet_id                            = "subnet-088ab46eb43b287ff"
+  tags = {
+    Name = "terraform"
+  }
+  tags_all = {
+    Name = "terraform"
+  }
+  tenancy                     = "default"
+  user_data                   = null
+  user_data_replace_on_change = null
+  volume_tags                 = null
+  vpc_security_group_ids      = ["sg-0730d36b107044e20"]
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+  cpu_options {
+    core_count       = 1
+    threads_per_core = 2
+  }
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+  enclave_options {
+    enabled = false
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 2
+    http_tokens                 = "required"
+    instance_metadata_tags      = "disabled"
+  }
+  private_dns_name_options {
+    enable_resource_name_dns_a_record    = true
+    enable_resource_name_dns_aaaa_record = false
+    hostname_type                        = "ip-name"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 8
+    volume_type           = "gp3"
+  }
 }
